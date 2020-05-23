f81c5c874554b43dbecf90dee89c4f626df07e25
serignefalloufall <fallfallou37@gmail.com>
2020-05-23 00:57:01 +0000

    update exo

--- exercice26.py
+++ exercice26.py
@@ -5,12 +5,12 @@ n = int(input())
 
 for i in range(1,n+1):
     print("Saisir un entier : ")
-    my_liste.append(int(input()))
+    my_liste.append(int(input())) # Remplissage de la liste
 
 
-print(my_liste)
+print(my_liste) # Affichage de la liste
 resultat = False
-elementPrecedent = my_liste[0]
+elementPrecedent = my_liste[0] # Recuperation de la premier element de la liste
 for element in my_liste:
     if(elementPrecedent > element):
         resultat=True
@@ -20,7 +20,7 @@ for element in my_liste:
         resultat=False
         elementPrecedent=element  
 
-print("prece : ",elementPrecedent,"elem : ",element)
+#print("prece : ",elementPrecedent,"elem : ",element)
 if (resultat == True):
     print("Est croissant")
 elif (resultat == False):
